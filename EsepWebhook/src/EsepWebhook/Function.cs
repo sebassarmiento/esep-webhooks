4cc009fda3fb4e4b06e9046176d8ef47d6f0c33f
Sebastian <sebi_sarmiento@hotmail.com>
2024-04-03 19:50:06 -0400

    Adding new code that fixes json parsing errors

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,36 +1,60 @@
+using System;
+using System.IO;
+using System.Net.Http;
 using System.Text;
 using Amazon.Lambda.Core;
-using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace EsepWebhook;
-
-public class Function
+namespace EsepWebhook
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
-    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public class Function
     {
-        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-        
-        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-        
-        var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
-        {
-            Content = new StringContent(payload, Encoding.UTF8, "application/json")
-        };
+        private static readonly HttpClient Client = new HttpClient();
 
-        var response = client.Send(webRequest);
-        using var reader = new StreamReader(response.Content.ReadAsStream());
-            
-        return reader.ReadToEnd();
+        public string FunctionHandler(GitHubEvent input, ILambdaContext context)
+        {
+            string message = $"Issue Created: {input.Issue.HtmlUrl}";
+            string payload = JsonSerializer.Serialize(new { text = message });
+
+            var slackUrl = Environment.GetEnvironmentVariable("SLACK_URL");
+            if (string.IsNullOrEmpty(slackUrl))
+            {
+                context.Logger.LogLine("Slack URL environment variable is not set.");
+                return "Error: Slack URL is not configured.";
+            }
+
+            var webRequest = new HttpRequestMessage(HttpMethod.Post, slackUrl)
+            {
+                Content = new StringContent(payload, Encoding.UTF8, "application/json")
+            };
+
+            var response = Client.Send(webRequest);
+            if (!response.IsSuccessStatusCode)
+            {
+                context.Logger.LogLine($"Failed to post message to Slack. Status code: {response.StatusCode}");
+                return "Error: Failed to send message to Slack.";
+            }
+
+            using var reader = new StreamReader(response.Content.ReadAsStream());
+            var responseContent = reader.ReadToEnd();
+            context.Logger.LogLine($"Message posted to Slack successfully. Response: {responseContent}");
+
+            return "Success";
+        }
+    }
+
+    public class GitHubEvent
+    {
+        public Issue Issue { get; set; }
+    }
+
+    public class Issue
+    {
+        [JsonPropertyName("html_url")]
+        public string HtmlUrl { get; set; }
     }
 }
--- EsepWebhook/src/EsepWebhook/obj/Debug/net8.0/EsepWebhook.AssemblyInfo.cs
+++ EsepWebhook/src/EsepWebhook/obj/Debug/net8.0/EsepWebhook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7042620fb6fa8c9e51caea9266034c15a5101216")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
